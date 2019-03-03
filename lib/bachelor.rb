6cbe3fb8d0ff25421423f703ce11be8a39038f68
Robert Tate <btate712@gmail.com>
2019-03-03 21:11:55 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -38,7 +38,14 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, info|
+    info.each do |contestant|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    end
+  end
+  nil 
 end
 
 def get_average_age_for_season(data, season)
